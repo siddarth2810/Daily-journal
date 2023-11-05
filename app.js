38c58819069de914c178c1292ec912917b65a6c7
Siddarth <110726331+siddarth2810@users.noreply.github.com>
2023-11-05 20:19:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const { title } = require('process');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
